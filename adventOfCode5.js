14b28cf33db4d1685372e06283f4d8ff23af3c8d
D0see <317Y17@gmail.com>
2025-03-03 23:52:46 +0100

    5 part 2

--- adventOfCode5.js
+++ adventOfCode5.js
@@ -31,7 +31,9 @@ const isValidOrder = (rulesObj, order) => {
   const currentOrder = [];
   for (let i = 0; i < order.length; i++) {
     currentOrder.forEach((page) => {
-      if (rulesObj[order[i]]?.[page]) isGoodOrder = false;
+      if (rulesObj[order[i]]?.[page]) {
+        isGoodOrder = false;
+      }
     });
     currentOrder.push(order[i]);
     if (!isGoodOrder) return false;
@@ -50,3 +52,37 @@ orders.forEach((order) => {
 });
 
 console.log(result);
+
+// PART 2
+
+const invalidOrders = orders.filter(order => !isValidOrder(rulesObj, order));
+
+const fixOrder = (rulesObj, order) => {
+  for (let i = 0; i < order.length; i++) {
+    const orderSlice = order.slice(0, i + 1);
+    if (isValidOrder(rulesObj, orderSlice)) continue;
+    const badPage = order[i];
+    for (let j = orderSlice.length - 2; j >= 0; j--) {
+      const temp = [...orderSlice].slice(0, orderSlice.length - 1);
+      temp.splice(j, 0, badPage);
+      if (!isValidOrder(rulesObj, temp)) continue;
+      order = temp.slice(0, i + 1).concat(order.slice(i + 1));
+      break;
+    }
+  }
+  return order;
+}
+
+//weird side effect with for invalidOrders.map but im too tired for this right now
+// invalidOrders.map(order => (fixOrder(rulesObj, order));
+
+let fixed = [];
+invalidOrders.forEach(order => fixed.push(fixOrder(rulesObj, order)));
+
+let result2 = 0;
+
+fixed.forEach((order) => {
+  if (isValidOrder(rulesObj, order)) result2 += parseInt(getMiddlePage(order));
+});
+
+console.log(result2);
