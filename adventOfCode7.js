cfde9e5d8b4ff859121438758e15b07f657d935a
D0see <317Y17@gmail.com>
2025-03-04 21:44:53 +0100

    add 7 input

---
+++ adventOfCode7.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+
+// INTUT GATHERING & CLEANING
+
+const rawInput = fs.readFileSync("./adventOfCode7Input.txt", {
+  encoding: "utf-8",
+});
+
+//get all perms kind of problems
