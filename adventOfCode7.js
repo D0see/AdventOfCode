6d029b9dc6b5243ac25d9bb4ff3a69f260429702
Leo LESUEUR <leo.lesueur2024@campus-eni.fr>
2025-03-05 13:40:16 +0100

    7 part 1

--- adventOfCode7.js
+++ adventOfCode7.js
@@ -6,4 +6,51 @@ const rawInput = fs.readFileSync("./adventOfCode7Input.txt", {
   encoding: "utf-8",
 });
 
-//get all perms kind of problems
+const operations = rawInput
+  .split("\r\n")
+  .map((line) => line.split(":"))
+  .map((line) => ({
+    result: parseInt(line[0]),
+    numbers: line[1]
+      .split(" ")
+      .slice(1)
+      .map((number) => parseInt(number)),
+  }));
+
+// PART 1
+
+const hasAWorkingPerm = (
+  operation,
+  result,
+  currNumIndex = 0,
+  currPerm = undefined
+) => {
+  if (result[0]) return;
+  if (currNumIndex === operation.numbers.length) {
+    if (currPerm === operation.result) result[0] = true;
+    return;
+  }
+  hasAWorkingPerm(
+    operation,
+    result,
+    currNumIndex + 1,
+    (currPerm === undefined ? 0 : currPerm) + operation.numbers[currNumIndex]
+  );
+  hasAWorkingPerm(
+    operation,
+    result,
+    currNumIndex + 1,
+    (currPerm === undefined ? 1 : currPerm) * operation.numbers[currNumIndex]
+  );
+};
+
+let result = 0;
+
+let hasAWorkingPermResultCatcher;
+operations.forEach((operation) => {
+  hasAWorkingPermResultCatcher = [false];
+  hasAWorkingPerm(operation, hasAWorkingPermResultCatcher);
+  if (hasAWorkingPermResultCatcher[0]) result += operation.result;
+});
+
+console.log(result);
