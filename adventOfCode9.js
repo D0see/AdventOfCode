521db3dc0959570d033cf09bae15f6ca711f8214
D0see <317Y17@gmail.com>
2025-03-07 20:44:20 +0100

    9 part 1

--- adventOfCode9.js
+++ adventOfCode9.js
@@ -5,3 +5,41 @@ const fs = require("fs");
 const rawInput = fs.readFileSync("./adventOfCode9Input.txt", {
   encoding: "utf-8",
 });
+
+//PART 1 (naive implementation)
+
+const litteralRep = [];
+for (let i = 0; i < rawInput.length; i++) {
+  if (i % 2) {
+    for (let j = 0; j < parseInt(rawInput[i]); j++) litteralRep.push(null);
+    continue;
+  }
+  for (let j = 0; j < parseInt(rawInput[i]); j++)
+    litteralRep.push(Math.floor(i / 2));
+}
+
+/* TESTING
+const litteralRep = "00...111...2...333.44.5555.6666.777.888899"
+  .split("")
+  .map((num) => (num === "." ? null : parseInt(num)));
+
+console.log(litteralRep);
+*/
+
+let result = 0;
+
+let rightPointer = litteralRep.length - 1;
+while (!litteralRep[rightPointer]) right--;
+
+for (let left = 0; left <= rightPointer; left++) {
+  if (litteralRep[left] !== null) {
+    result += litteralRep[left] * left;
+    continue;
+  }
+  result += litteralRep[rightPointer] * left;
+  do {
+    rightPointer--;
+  } while (litteralRep[rightPointer] === null);
+}
+
+console.log(result);
