16769672e002a8a9cbe7d1a6920d5fa8575863d3
llesueur2024 <leo.lesueur2024@campus-eni.fr>
2025-03-10 13:45:11 +0100

    started working on 9 part 2

--- adventOfCode9.js
+++ adventOfCode9.js
@@ -43,3 +43,49 @@ for (let left = 0; left <= right; left++) {
 }
 
 console.log(result);
+
+// PART 2
+
+const testArr = "00...111...2...333.44.5555.6666.777.888899";
+
+console.log(testArr);
+
+let result2 = 0;
+
+right = testArr.length - 1;
+
+//get First Box
+while (!testArr[right]) right--;
+rightLeft = right;
+while (testArr[rightLeft - 1] === testArr[right]) rightLeft--;
+let currBox = [rightLeft, right + 1];
+console.log(testArr.slice(currBox[0], currBox[1]));
+
+right = rightLeft;
+right--;
+while (!testArr[right]) right--;
+rightLeft = right;
+while (testArr[rightLeft - 1] === testArr[right]) rightLeft--;
+let currBox2 = [rightLeft, right + 1];
+console.log(testArr.slice(currBox2[0], currBox2[1]));
+
+// add an array that only contains the max possible space in case this function returns false
+const findFittingSpace = (length, str) => {
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === ".") {
+      let index = i;
+      while (str[index] === ".") index++;
+      const spaceLength = index - i;
+      if (length <= spaceLength) {
+        return [i, index];
+      }
+    }
+  }
+  return false;
+};
+
+console.log(findFittingSpace(2, testArr));
+
+console.log(findFittingSpace(3, testArr));
+
+console.log(findFittingSpace(4, testArr));
