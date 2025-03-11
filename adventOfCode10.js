3aa4a8482db8ea1ca5d8ede06bac6ddc7814b04d
llesueur2024 <leo.lesueur2024@campus-eni.fr>
2025-03-11 11:13:39 +0100

    10 part 2

--- adventOfCode10.js
+++ adventOfCode10.js
@@ -10,29 +10,20 @@ const map = rawInput
   .split("\r\n")
   .map((row) => row.split("").map((num) => parseInt(num)));
 
+// PART 1
+
 const getNumberOfAccessible9s = (map, y, x, result = {}, currNum = 0) => {
   if (currNum === 9) return (result[`${y}-${x}`] = true);
-  if (map[y]?.[x + 1] === currNum + 1) {
+  if (map[y]?.[x + 1] === currNum + 1)
     getNumberOfAccessible9s(map, y, x + 1, result, currNum + 1);
-  }
   if (map[y]?.[x - 1] === currNum + 1)
     getNumberOfAccessible9s(map, y, x - 1, result, currNum + 1);
-  if (map[y + 1]?.[x] === currNum + 1) {
+  if (map[y + 1]?.[x] === currNum + 1)
     getNumberOfAccessible9s(map, y + 1, x, result, currNum + 1);
-  }
   if (map[y - 1]?.[x] === currNum + 1)
     getNumberOfAccessible9s(map, y - 1, x, result, currNum + 1);
   return Object.keys(result).length;
 };
-
-/*
-const test =
-  "89010123.78121874.87430965.96549874.45678903.32019012.01329801.10456732"
-    .split(".")
-    .map((row) => row.split("").map((num) => parseInt(num)));
-
-console.log(getNumberOfAccessible9s(test, 0, 2));
-*/
 
 let result = 0;
 
@@ -43,3 +34,63 @@ for (let y = 0; y < map.length; y++) {
 }
 
 console.log(result);
+
+// PART 2
+
+const getNumberOfDifferentTrails = (
+  map,
+  y,
+  x,
+  currTrail = `${y}-${x}-`,
+  result = {},
+  currNum = 0
+) => {
+  if (currNum === 9) return (result[currTrail] = true);
+  if (map[y]?.[x + 1] === currNum + 1)
+    getNumberOfDifferentTrails(
+      map,
+      y,
+      x + 1,
+      currTrail + `-${y}-${x + 1}-`,
+      result,
+      currNum + 1
+    );
+  if (map[y]?.[x - 1] === currNum + 1)
+    getNumberOfDifferentTrails(
+      map,
+      y,
+      x - 1,
+      currTrail + `-${y}-${x - 1}-`,
+      result,
+      currNum + 1
+    );
+  if (map[y + 1]?.[x] === currNum + 1)
+    getNumberOfDifferentTrails(
+      map,
+      y + 1,
+      x,
+      currTrail + `-${y + 1}-${x}-`,
+      result,
+      currNum + 1
+    );
+  if (map[y - 1]?.[x] === currNum + 1)
+    getNumberOfDifferentTrails(
+      map,
+      y - 1,
+      x,
+      currTrail + `-${y - 1}-${x}-`,
+      result,
+      currNum + 1
+    );
+  return Object.keys(result).length;
+};
+
+let result2 = 0;
+
+for (let y = 0; y < map.length; y++) {
+  for (let x = 0; x < map[0].length; x++) {
+    if (!map[y][x]) result2 += getNumberOfDifferentTrails(map, y, x);
+  }
+}
+
+console.log(result2);
