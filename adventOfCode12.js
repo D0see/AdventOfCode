9e79f7c28b4e5fa73723021e11f3eb9690d5b322
D0see <317Y17@gmail.com>
2025-03-12 23:19:40 +0100

    12 part 2

--- adventOfCode12.js
+++ adventOfCode12.js
@@ -35,12 +35,45 @@ const floodFill = (y, x, map, vege = map[y][x], plotStats = {area : 0, perimeter
     return plotStats;
 }
 
-//COUNTSIDES USING FENCESPOSITIONS -> ORIENTATIONS MAP FOR PART 2
+//COUNTSIDES USING FENCESPOSITIONS -> ORIENTATIONS MAP FOR PART 2 (this feels really stupid)
 const countSides = (fencePositions) => {
-
+    let result = 0;
+    for (const key of Object.keys(fencePositions)) {
+        const [y, x] = key.split('-').map(coordinate => parseInt(coordinate));
+        for (const orientation of Object.keys(fencePositions[key])) {
+            if (!fencePositions[key][orientation]) continue;
+            fencePositions[key[orientation]] = false;
+            if (["UP", "DOWN"].includes(orientation)) {
+                let val = 1;
+                while(fencePositions[`${y}-${x + val}`]?.[orientation]) {
+                    fencePositions[`${y}-${x + val}`][orientation] = false;
+                    val++;
+                }
+                val = -1;
+                while(fencePositions[`${y}-${x + val}`]?.[orientation]) {
+                    fencePositions[`${y}-${x + val}`][orientation] = false;
+                    val--;
+                }
+            } else if (["LEFT", "RIGHT"].includes(orientation)) {
+                let val = 1;
+                while(fencePositions[`${y + val}-${x}`]?.[orientation]) {
+                    fencePositions[`${y + val}-${x}`][orientation] = false;
+                    val++;
+                }
+                val = -1;
+                while(fencePositions[`${y + val}-${x}`]?.[orientation]) {
+                    fencePositions[`${y + val}-${x}`][orientation] = false;
+                    val--;
+                }
+            }
+            result++;
+        }
+    }
+    return result;
 }
 
 let result = 0;
+let result2 = 0;
 
 for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[0].length; x++) {
@@ -48,12 +81,14 @@ for (let y = 0; y < map.length; y++) {
             // plotStats = {area : int, perimeter : int}
             const plotStats = floodFill(y, x, map);
             result += plotStats.area * plotStats.perimeter;
-            countSides(plotStats.fencePositions);
+            result2 += countSides(plotStats.fencePositions) * plotStats.area;
         } 
     }
 }
 
 console.log(result);
+console.log(result2);
+
 
 // PART 2
 
