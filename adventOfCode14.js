90bd2575ac15efe2a6266ae4fc5b28694f84aaec
D0see <317Y17@gmail.com>
2025-03-14 22:39:12 +0100

    14 part 2 looks insane

--- adventOfCode14.js
+++ adventOfCode14.js
@@ -24,10 +24,11 @@ for (const infos of splittedByLines) {
     })
 }
 
-
-//PART 1
 const tilesWide = 101;
 const tilesTall = 103;
+
+//PART 1
+
 
 const robotsPositionsAfter100s = [];
 
@@ -39,28 +40,54 @@ for (const robot of robots) {
 }
 
 let quadrants = [0,0,0,0]
-
 for (const robot of robotsPositionsAfter100s) {
     if (robot.x === Math.floor(tilesWide / 2) ||
-        robot.y === Math.floor(tilesTall / 2)) {
-            continue;
-        }
+        robot.y === Math.floor(tilesTall / 2)) continue;
+
     if (robot.x < Math.floor(tilesWide / 2)) {
-        if (robot.y < Math.floor(tilesTall / 2)) {
-            quadrants[0]++;
-        } else {
-            quadrants[1]++;
-        }
-    } else if (robot.x > Math.floor(tilesWide / 2)) {
-        if (robot.y < Math.floor(tilesTall / 2)) {
-            quadrants[2]++;
-        } else {
-            quadrants[3]++;
-        }
+        robot.y < Math.floor(tilesTall / 2) ? quadrants[0]++ : quadrants[1]++;
+        continue;
+    } 
+    robot.y < Math.floor(tilesTall / 2) ? quadrants[2]++ : quadrants[3]++;
+    
+}
+console.log(quadrants.reduce((acc, val) => acc *= val, 1));
+
+//PART 2 ATTEMPT AT VISUALIZATION (nope)
+const matrix = [];
+for (let y = 0; y < tilesTall; y++) {
+    const currRow = [];
+    for (let x = 0; x < tilesWide; x++) {
+        currRow.push(0);
     }
+    matrix.push(currRow);
 }
 
-console.log(quadrants.reduce((acc, val) => {
-    acc *= val;
-    return acc;
-} , 1));
+
+
+//initializes matrix
+for (const robot of robots) {
+    matrix[robot.position.y][robot.position.x] += 1;
+}
+
+//animationLoop
+const hasTrunk = (matrix) => {
+    for (let i = 0; i < 25; i++) {
+        if (matrix[102 - i][50]) continue;
+        return false;
+    }
+    return true;
+}
+
+while(true) {
+    for (const robot of robots) {
+        matrix[robot.position.y][robot.position.x] -= 1;
+        matrix
+        [(((robot.position.y + (robot.velocity.y *100 )) % tilesTall) + tilesTall) % tilesTall]
+        [(((robot.position.x + (robot.velocity.x)) % tilesWide) + tilesWide) % tilesWide] += 1;
+    }
+    if (hasTrunk(matrix)) {
+        console.log("trunk alert");
+        break;
+    }
+}
