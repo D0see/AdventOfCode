ebb73becdee823489846d18057f56a2a699f08a6
llesueur2024 <leo.lesueur2024@campus-eni.fr>
2025-04-14 10:10:26 +0200

    15 part 2 horizontal box movement

--- adventOfCode15.js
+++ adventOfCode15.js
@@ -10,25 +10,6 @@ const splittedByLines = rawInput.split("\r\n");
 
 const separation = splittedByLines.indexOf("");
 
-const board = splittedByLines
-  .slice(0, separation)
-  .map((line) => line.split(""));
-const moves = splittedByLines
-  .slice(separation + 1)
-  .map((line) => line.split(""))
-  .flat();
-
-const initialPosition = {};
-
-yLoop: for (let y = 0; y < board.length; y++) {
-  for (let x = 0; x < board[0].length; x++) {
-    if (board[y][x] === "@") {
-      (initialPosition.posX = x), (initialPosition.posY = y);
-      break yLoop;
-    }
-  }
-}
-
 const moveMap = {
   "^": {
     y: -1,
@@ -52,6 +33,37 @@ const updatePosition = (previousPos, nextPos) => {
   previousPos.posX = nextPos.posX;
   previousPos.posY = nextPos.posY;
 };
+
+const getGPSScore = (board) => {
+  let result = 0;
+  for (let y = 0; y < board.length; y++) {
+    for (let x = 0; x < board[0].length; x++) {
+      if (board[y][x] === "O") result += 100 * y + x;
+    }
+  }
+  return result;
+}
+
+//PART 1
+
+const board = splittedByLines
+  .slice(0, separation)
+  .map((line) => line.split(""));
+const moves = splittedByLines
+  .slice(separation + 1)
+  .map((line) => line.split(""))
+  .flat();
+
+const initialPosition = {};
+
+yLoop: for (let y = 0; y < board.length; y++) {
+  for (let x = 0; x < board[0].length; x++) {
+    if (board[y][x] === "@") {
+      (initialPosition.posX = x), (initialPosition.posY = y);
+      break yLoop;
+    }
+  }
+}
 
 const UpdateBoard = (board, previousPos, moveMap, move) => {
   const nextPos = {
@@ -85,15 +97,116 @@ const UpdateBoard = (board, previousPos, moveMap, move) => {
   }
 };
 
-for (const move of moves) {
-  UpdateBoard(board, initialPosition, moveMap, move);
-}
+for (const move of moves) UpdateBoard(board, initialPosition, moveMap, move);
 
-let result = 0;
-for (let y = 0; y < board.length; y++) {
+// RESULT
+console.log(getGPSScore(board));
+
+// PART 2
+
+// If the tile is #, the new map contains ## instead.
+// If the tile is O, the new map contains [] instead.
+// If the tile is ., the new map contains .. instead.
+// If the tile is @, the new map contains @. instead.
+
+const normalBoard = splittedByLines
+  .slice(0, separation)
+  .map((line) => line.split(""));
+
+const doubledBoard = [];
+
+normalBoard.forEach(row =>{
+  const currRow = [];
+  row.forEach(char => {
+    switch(char) {
+      case '#' :
+        currRow.push('#','#');
+        break;
+      case 'O' :
+        currRow.push('[',']');
+        break;
+      case '.' :
+        currRow.push('.','.');
+        break;
+      case '@' :
+        currRow.push('@','.');
+        break;
+    }
+  })
+  doubledBoard.push(currRow);
+})
+
+
+const startingPosition = {};
+
+yLoop: for (let y = 0; y < board.length; y++) {
   for (let x = 0; x < board[0].length; x++) {
-    if (board[y][x] === "O") result += 100 * y + x;
+    if (board[y][x] === "@") {
+      (startingPosition.posX = x), (startingPosition.posY = y);
+      break yLoop;
+    }
   }
 }
 
-console.log(result);
+const moveHorizontally = (board, previousPos, nextPos, moveMap, move) => {
+  let offsetX = 0;
+  while (board[nextPos.posY][nextPos.posX + offsetX] !== "#") {
+    const currLookUp = board[nextPos.posY][nextPos.posX + offsetX];
+    if (currLookUp === ".") {
+      board[nextPos.posY][nextPos.posX + offsetX] = move === ">" ? "]" : "[";
+      let leftBracket = board[nextPos.posY][nextPos.posX + offsetX] = "[";
+      while (offsetX) {
+        Math.sign(offsetX) === 1 ? offsetX-- : offsetX++; 
+        board[nextPos.posY][nextPos.posX + offsetX] = leftBracket ? ']' : '[';
+        leftBracket = !leftBracket;
+      }
+      board[nextPos.posY][nextPos.posX] = "@";
+      board[previousPos.posY][previousPos.posX] = ".";
+      updatePosition(previousPos, nextPos);
+      break;
+    }
+    offsetX += moveMap[move].x;
+  }
+}
+
+const goToNextStep = (board, previousPos, moveMap, move) => {
+  const nextPos = {
+    posY: previousPos.posY + moveMap[move].y,
+    posX: previousPos.posX + moveMap[move].x,
+  };
+  console.log(nextPos)
+  // if next move leads to a wall
+  if (board[nextPos.posY][nextPos.posX] === "#") return;
+  // if next move leads to an empty space
+  if (board[nextPos.posY][nextPos.posX] === ".") {
+    board[nextPos.posY][nextPos.posX] = "@";
+    board[previousPos.posY][previousPos.posX] = ".";
+    updatePosition(previousPos, nextPos);
+  }
+  // if next move leads to a box
+  console.log(move, "move")
+  console.log("next Symbol", board[nextPos.posY][nextPos.posX])
+  if (['[',']'].includes(board[nextPos.posY][nextPos.posX])) {
+    console.log("passes1")
+    switch (move) {
+        case "^": 
+        case ">":
+          moveHorizontally(board, previousPos, nextPos, moveMap, move);
+          break;
+        case "v": 
+        case "<":
+          moveHorizontally(board, previousPos, nextPos, moveMap, move);
+          console.log("passes2")
+          break;
+    }
+  }
+}
+
+const testRow = [["#","#",".",".",".",".","[","]","[","]","@",".","#","#"]];
+const testMoves = ['<','<','<','<','<','<'];
+
+const testInitialPos = {posX: 10,posY: 0,}
+for (const move of testMoves) {goToNextStep(testRow, testInitialPos, moveMap, move)
+  console.log(testRow)
+}
+
