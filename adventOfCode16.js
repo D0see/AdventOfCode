fb72ca3da6dde20ed48a0fcc0c0e6d361c9468f6
llesueur2024 <leo.lesueur2024@campus-eni.fr>
2025-04-15 09:34:04 +0200

    16 part 1

--- adventOfCode16.js
+++ adventOfCode16.js
@@ -10,39 +10,23 @@ const splittedByLines = rawInput.split("\r\n");
 
 const board = splittedByLines.map(row => row.split(""));
 
-const testBoard = [
-    ['#','#','#','#','#','#','#','#','#'],
-    ['#','.','.','#','.','.','.','E','#'],
-    ['#','.','.','#','.','#','.','.','#'],
-    ['#','S','.','.','.','#','.','.','#'],
-    ['#','#','#','#','#','#','#','#','#']
-]
+//PART 1
 
 //recursive function to get to E
-const traverseboard = (board, y, x, orientation, currResult, result, visited = {}) => {
-    console.log(result);
+const traverseboard = (board, y, x, orientation, currResult, result, minScore = {}) => {
+
     if (board[y][x] === 'E') {
         if (!result[0]) result[0] = currResult;
         else if (currResult < result[0]) result[0] = currResult; 
     };
-    if (result[0] && currResult > result[0]) return
 
-    visited[`${y}-${x}`] = true;
+    if (minScore[`${y}-${x}`] <= (currResult)) {return;}
+    minScore[`${y}-${x}`] = currResult;
 
-    if (board[y - 1][x] !== '#' && !visited[`${y - 1}-${x}`]) {
-        traverseboard(board, y - 1, x, 'N', currResult + (orientation === 'N' ? 1 : 1001), result, JSON.parse(JSON.stringify(visited)))
-    }
-    if (board[y + 1][x] !== '#' && !visited[`${y + 1}-${x}`]) {
-        traverseboard(board, y + 1, x, 'S', currResult + (orientation === 'S' ? 1 : 1001), result, JSON.parse(JSON.stringify(visited)))
-    }
-    if (board[y][x - 1] !== '#' && !visited[`${y}-${x - 1}`]) {
-        traverseboard(board, y, x - 1, 'W', currResult + (orientation === 'W' ? 1 : 1001), result, JSON.parse(JSON.stringify(visited)))
-    }
-    if (board[y][x + 1] !== '#' && !visited[`${y}-${x + 1}`]) {
-        traverseboard(board, y, x + 1, 'E', currResult + (orientation === 'E' ? 1 : 1001), result, JSON.parse(JSON.stringify(visited)))
-    }
-
-
+    if (board[y - 1][x] !== '#') traverseboard(board, y - 1, x, 'N', currResult + (orientation === 'N' ? 1 : 1001), result, minScore)
+    if (board[y][x + 1] !== '#') traverseboard(board, y, x + 1, 'E', currResult + (orientation === 'E' ? 1 : 1001), result, minScore)
+    if (board[y + 1][x] !== '#') traverseboard(board, y + 1, x, 'S', currResult + (orientation === 'S' ? 1 : 1001), result, minScore)
+    if (board[y][x - 1] !== '#') traverseboard(board, y, x - 1, 'W', currResult + (orientation === 'W' ? 1 : 1001), result, minScore)
 }
 
 let result = [0];
