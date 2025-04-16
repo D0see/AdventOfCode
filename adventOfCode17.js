897e983f2e1f7583cb5717286e7e2dfe78826a33
llesueur2024 <leo.lesueur2024@campus-eni.fr>
2025-04-16 12:23:45 +0200

    18 part 1

--- adventOfCode17.js
+++ adventOfCode17.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-const { off } = require("process");
 
 // INTUT GATHERING & CLEANING
 
---
+++ adventOfCode18.js
@@ -0,0 +1,55 @@
+const fs = require("fs");
+
+// INTUT GATHERING & CLEANING
+
+const rawInput = fs.readFileSync("./adventOfCode18Input.txt", {
+  encoding: "utf-8",
+});
+
+// x, y coords
+const splittedByLines = rawInput.split("\r\n");
+const coordinatesArrays = splittedByLines.map(line => line.split(','));
+
+
+// PART 1
+
+// coordinate obj -> {...y: {...x}}
+const coordinates = coordinatesArrays.slice(0, 1024).reduce((obj, coord) => {
+    // corrupted coordinates are initialized at -Infinity
+    obj[coord[1]] ? obj[coord[1]][coord[0]] = -Infinity : obj[coord[1]] = {[coord[0]] : -Infinity};
+    return obj;
+}, {})
+
+const shortestPath = [0];
+
+// recursive function to find the shortest path
+const findShortestPath = (shortestPath, coordinates, y, x, currPathLength, destinationY, destinationX) => {
+    coordinates[y] ? coordinates[y][x] = currPathLength : coordinates[y] = {[x] : currPathLength};
+
+    if (x === destinationX && y === destinationY) 
+        return shortestPath[0] = shortestPath[0] < currPathLength && shortestPath[0] ? shortestPath[0] : currPathLength;
+
+    [
+        [y + 1, x],
+        [y, x + 1],
+        [y - 1, x],
+        [y, x - 1],
+    ].forEach(([y, x]) => {
+        if (y < 0 || y > destinationY || x < 0 || x > destinationX || (coordinates[y]?.[x] <= (currPathLength + 1))) return;
+        findShortestPath(shortestPath, coordinates, y, x, currPathLength + 1, destinationY, destinationX);
+    })
+    
+}
+
+findShortestPath(shortestPath, coordinates, 0,0,0,70,70);
+console.log(shortestPath[0])
+
+
+// const testShortestPath = [0]
+// const testCoord = {
+//     0 : {
+//         1 : -Infinity
+//     }
+// }
+// findShortestPath(testShortestPath, testCoord, 0,0,0,1,1);
+// console.log(testShortestPath[0])
