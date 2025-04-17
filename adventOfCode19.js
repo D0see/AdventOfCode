914ff71ca8fd0451684bdf66b23321d7504c5103
D0see <317Y17@gmail.com>
2025-04-17 08:14:26 +0200

    19 part 1 attempt

--- adventOfCode19.js
+++ adventOfCode19.js
@@ -7,38 +7,62 @@ const rawInput = fs.readFileSync("./adventOfCode19Input.txt", {
 });
 
 const splittedByLines = rawInput.split("\r\n");
-const indexSeparation = splittedByLines.indexOf(""); 
+const indexSeparation = splittedByLines.indexOf("");
 
 const arrayOfBricks = splittedByLines[0].split(", ");
 const bricks = arrayOfBricks.reduce((obj, val) => {
-    obj[val] = true;
-    return obj;
-}, {})
+  obj[val] = true;
+  return obj;
+}, {});
 
-const maxBrickLength = Math.max(...arrayOfBricks.map(brick => brick.length));
+const maxBrickLength = Math.max(...arrayOfBricks.map((brick) => brick.length));
 
 const orders = splittedByLines.slice(indexSeparation + 1);
 
-const OrderIsValid = (order, bricks, maxBrickLength, currIndex = 0, isValid = [false]) => {
-    if (isValid[0]) return;
-    if (currIndex === order.length) {return isValid[0] = true;}
-    for (let i = currIndex; i < Math.min((currIndex + maxBrickLength), order.length); i++) {
-        const currBrick = order.slice(currIndex, i + 1);
-        console.log(currBrick, "<- test brick");
-        console.log(bricks[currBrick]);
-        if (bricks[currBrick]) {
-            console.log(order.slice(i + 1), " left");
-            console.log("_________________")
-            OrderIsValid(order.slice(i + 1), bricks, maxBrickLength, i + 1, isValid);
-        }
+const impossibleOrders = {};
+
+const OrderIsValid = (
+  order,
+  bricks,
+  maxBrickLength,
+  result = [false],
+  impossibleOrders
+) => {
+  if (result[0]) return result[0];
+  if (!order.length) {
+    return (result[0] = true);
+  }
+  const iterator = Math.min(order.length, maxBrickLength);
+  for (let i = 1; i <= iterator; i++) {
+    const currBrick = order.slice(0, i);
+    if (bricks[currBrick]) {
+      OrderIsValid(
+        order.slice(i),
+        bricks,
+        maxBrickLength,
+        result,
+        impossibleOrders
+      );
     }
-    return isValid[0];
-}
+  }
+  return result[0];
+};
+
+// let result = 0;
+// for (const order of orders) {
+//   console.count("order");
+//   if (OrderIsValid(order, bricks, maxBrickLength, impossibleOrders)) result++;
+// }
 
 let result = 0;
-console.log(orders[0]);
-for (const order of orders) {
-    if(OrderIsValid(order, bricks, maxBrickLength)) result++;
-}
-console.log(orders[0]);
+if (
+  OrderIsValid(
+    "uwrbubuwrwgbubguguurwwurbrurbwbwwwgbrwwwg",
+    bricks,
+    8,
+    impossibleOrders
+  )
+)
+  result++;
+
 console.log(result);
