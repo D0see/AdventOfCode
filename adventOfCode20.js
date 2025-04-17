865199340c0dd89863012bac97d80151f081308f
D0see <317Y17@gmail.com>
2025-04-17 22:41:04 +0200

    progress on 20 part 2

--- adventOfCode20.js
+++ adventOfCode20.js
@@ -7,40 +7,41 @@ const rawInput = fs.readFileSync("./adventOfCode20Input.txt", {
 });
 
 const splittedByLines = rawInput.split("\r\n");
-const map = splittedByLines.map(row => row.split(''));
+const map = splittedByLines.map((row) => row.split(""));
 
 const findPosition = (map, char) => {
-    for (let y = 0; y < map.length; y++) {
-        for (let x = 0; x < map[0].length; x++) {
-            if (map[y][x] === char) return [y, x];
-        }
+  for (let y = 0; y < map.length; y++) {
+    for (let x = 0; x < map[0].length; x++) {
+      if (map[y][x] === char) return [y, x];
     }
-}
+  }
+};
 
-const [startY, startX] = findPosition(map, 'S');
+const [startY, startX] = findPosition(map, "S");
 
 // PART 1
 
 // BFS
 const findShortestPath = (map, y, x, alreadyVisited = {}) => {
-    //initializes first square
-    const memo = [[y, x, 0]];
-    while (memo.length) {
-        const [currY, currX, score] = memo.shift();
-        if (map[currY][currX] === 'E') return score;
-        alreadyVisited[`${currY}-${currX}`] = true;
-        [
-            [currY + 1, currX, score + 1],
-            [currY, currX + 1, score + 1],
-            [currY - 1, currX, score + 1],
-            [currY, currX - 1, score + 1],
-        ].forEach(([y, x, nextScore]) => {
-            if (!map[y]?.[x] || map[y][x] === '#' || alreadyVisited[`${y}-${x}`]) return;
-            memo.push([y, x, nextScore])
-        })
-    }
-    return 0;
-}
+  //initializes first square
+  const memo = [[y, x, 0]];
+  while (memo.length) {
+    const [currY, currX, score] = memo.shift();
+    if (map[currY][currX] === "E") return score;
+    alreadyVisited[`${currY}-${currX}`] = true;
+    [
+      [currY + 1, currX, score + 1],
+      [currY, currX + 1, score + 1],
+      [currY - 1, currX, score + 1],
+      [currY, currX - 1, score + 1],
+    ].forEach(([y, x, nextScore]) => {
+      if (!map[y]?.[x] || map[y][x] === "#" || alreadyVisited[`${y}-${x}`])
+        return;
+      memo.push([y, x, nextScore]);
+    });
+  }
+  return 0;
+};
 
 // const fastestPathNoCheats = findShortestPath(map, startY, startX);
 
@@ -59,33 +60,64 @@ const findShortestPath = (map, y, x, alreadyVisited = {}) => {
 
 // console.log(result);
 
-
-
 // PART 2
 
 // find the cost to reach every square from the start
+
+const [endingY, endingX] = findPosition(map, "E");
+// removes the original ending
+map[endingY][endingX] = ".";
+
+const costToReachFromStart = {};
+
+//builds an hashmap of the cost of reaching every square from the start
+for (let y = 1; y < map.length - 1; y++) {
+  for (let x = 1; x < map[0].length - 1; x++) {
+    if (map[y][x] === "#" || map[y][x] === "S") continue;
+    map[y][x] = "E";
+    const fastestPath = findShortestPath(map, startY, startX);
+    costToReachFromStart[y]
+      ? (costToReachFromStart[y][x] = fastestPath)
+      : (costToReachFromStart[y] = { [x]: fastestPath });
+    map[y][x] = ".";
+  }
+}
+console.log(costToReachFromStart);
+
+map[endingY][endingX] = "E";
+
 // find the cost to reach the end from every square
+
+// removes the original start
+map[startY][startX] = ".";
+
+const costToReachEndFrom = {};
+
+//builds an hashmap of the cost of reaching the end from evert square
+for (let y = 1; y < map.length - 1; y++) {
+  for (let x = 1; x < map[0].length - 1; x++) {
+    if (map[y][x] === "#" || map[y][x] === "E") continue;
+    map[y][x] = "S";
+    const fastestPath = findShortestPath(map, y, x);
+    costToReachEndFrom[y]
+      ? (costToReachEndFrom[y][x] = fastestPath)
+      : (costToReachEndFrom[y] = { [x]: fastestPath });
+    map[y][x] = ".";
+  }
+}
+console.log(costToReachEndFrom);
+
+map[startY][startX] = "S";
 // find the lowest score when adding the first two (at most at 20 distance)
 
-for (let y = 1; y < map.length - 1; y++) {
-    for (let x = 1; x < map[0].length - 1; x++) {
-        if (map[y][x] !== '#') continue;
-        console.log(y," ", x)
-        map[y][x] = '.';
-        const fastestPath = findShortestPath(map, startY, startX);
-        if ((fastestPathNoCheats - fastestPath) >= 100) result++;
-        map[y][x] = '#'
-    }
-}
+// const test = [
+//   ["#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"],
+//   ["#", "S", ".", "#", ".", ".", ".", ".", ".", ".", "#"],
+//   ["#", ".", ".", ".", ".", ".", "#", ".", ".", ".", "#"],
+//   ["#", ".", ".", ".", ".", ".", "#", ".", ".", "E", "#"],
+//   ["#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"],
+// ];
 
-const test = [
-    ['#','#','#','#','#','#','#','#','#','#','#'],
-    ['#','S','.','#','.','.','.','.','.','.','#'],
-    ['#','.','.','.','.','.','#','.','.','.','#'],
-    ['#','.','.','.','.','.','#','.','.','E','#'],
-    ['#','#','#','#','#','#','#','#','#','#','#'],
-]
+// const visitedInBestPath = findShortestPath(test, 1, 1);
 
-const visitedInBestPath = getShortestPath(test, 1, 1);
-
-console.log(visitedInBestPath)
+// console.log(visitedInBestPath);
