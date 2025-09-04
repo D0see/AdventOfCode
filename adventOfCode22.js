799cc5d5bd8d670be71088eb84b3aca7f8760890
D0see <317Y17@gmail.com>
2025-09-04 10:36:43 +0200

    22 part 2

--- adventOfCode22.js
+++ adventOfCode22.js
@@ -8,6 +8,9 @@ const rawInput = fs.readFileSync("./adventOfCode22Input.txt", {
 
 const numbers = rawInput.split('\r\n');
 
+
+//PART 1
+
 const pruneVal = BigInt(16777216);
 
 const generateNextNum = (number) => {
@@ -39,3 +42,36 @@ for(let i = 0; i < numbers.length; i++) {
 }
 
 console.log(result);
+
+//PART 2 
+
+const sequencesVal = {};
+
+for(let i = 0; i < numbers.length; i++) {
+    let currNum = numbers[i];
+    const lastDigits = [`${currNum}`[`${currNum}`.length - 1]];
+    const currBuyerSequenceVal = {};
+
+    for (let j = 0; j < 2000; j++) {
+        currNum = generateNextNum(currNum); 
+        lastDigits.push(`${currNum}`[`${currNum}`.length - 1])
+    }
+
+    for (let i = 4; i < lastDigits.length; i++) {
+        let firstVal = lastDigits[i - 3] - lastDigits[i - 4];
+        let secondVal = lastDigits[i - 2] - lastDigits[i - 3];
+        let thirdVal = lastDigits[i - 1] - lastDigits[i - 2];
+        let fourthVal = lastDigits[i] - lastDigits[i - 1];
+
+        const sequence = `${firstVal}/${secondVal}/${thirdVal}/${fourthVal}`
+        currBuyerSequenceVal[sequence] ? '' : currBuyerSequenceVal[sequence] = Number(lastDigits[i]);
+    }
+
+    for (const key of Object.keys(currBuyerSequenceVal)) {
+        sequencesVal[key] ? sequencesVal[key] += currBuyerSequenceVal[key] : sequencesVal[key] = currBuyerSequenceVal[key]
+    }
+    
+}
+
+console.log(Object.values(sequencesVal).reduce((acc, val) => val > acc ? val : acc,0));
+
