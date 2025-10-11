d1593320790b1babd565c8135cee93dc5ba67f66
D0see <317Y17@gmail.com>
2025-10-11 11:42:43 +0200

    2022 6 part 2

---
+++ 2022/adventOfCode6.php
@@ -0,0 +1,31 @@
+<?php
+    declare(strict_types=1);
+
+    //input parsing
+    $fh = fopen('adventOfCode6Input.txt','r');
+    $inputString = '';
+    while ($line = fgets($fh)) {
+        $inputString .= $line; 
+    }
+    fclose($fh);
+
+    //PART 1
+    function getFirstMarkerAppearanceEndIndex(string &$inputString, int $markerLength): int {
+        for ($i = 0; $i < (strlen($inputString) - $markerLength); $i++) {
+            $markerCharacters = [];
+            for ($j = 0; $j < $markerLength; $j++) {
+                $markerCharacters[$inputString[$i + $j]] = true;
+            }
+            if (count($markerCharacters) === $markerLength) {
+                return $i + $markerLength;
+            }
+        }
+        return -1;
+    }
+
+    echo getFirstMarkerAppearanceEndIndex($inputString, 4);
+    echo "<br>";
+
+    //PART 2
+    echo getFirstMarkerAppearanceEndIndex($inputString, 14);
+?>
