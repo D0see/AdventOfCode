267c905978b4537825d55e92a04344de642e23c8
D0see <317Y17@gmail.com>
2025-12-02 22:55:46 +0100

    this is a horrible way to start this one ahha

--- 2025/adventOfCode1.php
+++ 2025/adventOfCode1.php
@@ -9,5 +9,27 @@
         array_push($inputArr, $line);
     }
     fclose($fh);
+
+    $result = 0;
+
+    $currNum = 50;
+
+    foreach($inputArr as $arr) {
+        $arr = str_split($arr);
+        $dir = $arr[0];
+        $amount = (int) implode('', array_slice($arr, 1));
+
+        if ($dir === 'L') {
+            $currNum -= $amount;
+        } else {
+            $currNum += $amount;
+        }
+
+        if ($currNum % 100 === 0) {
+            $result++;
+        }
+    }
+
+    echo $result;
 ?>
 
