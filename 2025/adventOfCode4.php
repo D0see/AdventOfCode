33201eed6c4c2c4a9be749aee1a825bb18059c30
D0see <317Y17@gmail.com>
2025-12-16 22:02:47 +0100

    aoc4 2025 part 2

---
+++ 2025/adventOfCode4.php
@@ -0,0 +1,86 @@
+<?php
+    declare(strict_types=1);
+
+    //input parsing
+    $inputArr = [];
+    $fh = fopen('adventOfCode4Input.txt','r');
+    while ($line = fgets($fh)) {
+        $line = str_split(trim($line));
+        array_push($inputArr, $line);
+    }
+    fclose($fh);
+
+    $result = 0;
+
+    // PART 1
+    for ($y = 0; $y < count($inputArr); $y++) {
+        for ($x = 0; $x < count($inputArr[0]); $x++) {
+            if ($inputArr[$y][$x] !== '@') continue;
+
+            $adjacencyScore = 0;
+
+            foreach( [
+                [-1, -1],[-1, 0],[-1, 1],[0, -1],[0, 1],[1, -1],[1, 0],[1, 1]
+            ] as $offsetArr) {
+
+                [$yOffset, $xOffset] = $offsetArr;
+
+                if (
+                    $y + $yOffset < 0 
+                    || $x + $xOffset < 0 
+                    || $y + $yOffset >= count($inputArr) 
+                    || $x + $xOffset >= count($inputArr[0])
+                ) continue;
+
+                if ($inputArr[$y + $yOffset][$x + $xOffset] === '@') $adjacencyScore++;
+
+            }
+
+            if ($adjacencyScore < 4) $result++;
+        }
+    }
+
+    echo $result;
+
+    echo '<br>';
+    
+    // PART 2
+    $result = 0;
+
+    $removedRolls = -1;
+
+    while($removedRolls !== 0) {
+        $removedRolls = 0;
+        for ($y = 0; $y < count($inputArr); $y++) {
+            for ($x = 0; $x < count($inputArr[0]); $x++) {
+                if ($inputArr[$y][$x] !== '@') continue;
+
+                $adjacencyScore = 0;
+
+                foreach( [
+                    [-1, -1],[-1, 0],[-1, 1],[0, -1],[0, 1],[1, -1],[1, 0],[1, 1]
+                ] as $offsetArr) {
+
+                    [$yOffset, $xOffset] = $offsetArr;
+
+                    if (
+                        $y + $yOffset < 0 
+                        || $x + $xOffset < 0 
+                        || $y + $yOffset >= count($inputArr) 
+                        || $x + $xOffset >= count($inputArr[0])
+                    ) continue;
+
+                    if ($inputArr[$y + $yOffset][$x + $xOffset] === '@') $adjacencyScore++;
+
+                }
+                
+                if ($adjacencyScore < 4) {
+                    $result++;
+                    $inputArr[$y][$x] = '.';
+                    $removedRolls++;
+                }
+            }
+        }
+    }
+
+    echo $result;
